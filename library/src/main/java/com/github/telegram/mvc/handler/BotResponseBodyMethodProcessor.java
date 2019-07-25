b16a3d697b254ff32d6b15ada4530bc253183f11
NikiGreen <antimagantimagov@mail.ru>
2019-07-26 00:00:48 +0300

    Ready to talk with telegram

---
+++ library/src/main/java/com/github/telegram/mvc/handler/BotResponseBodyMethodProcessor.java
@@ -0,0 +1,44 @@
+package com.github.telegram.mvc.handler;
+
+import com.github.telegram.mvc.api.TelegramRequest;
+import com.pengrad.telegrambot.request.SendMessage;
+import org.springframework.core.MethodParameter;
+import org.springframework.core.convert.ConversionService;
+
+public class BotResponseBodyMethodProcessor implements BotHandlerMethodReturnValueHandler {
+    private ConversionService conversionService;
+
+    public BotResponseBodyMethodProcessor(ConversionService conversionService) {
+
+        this.conversionService = conversionService;
+    }
+
+    @Override
+    public boolean supportsReturnType(MethodParameter returnType) {
+        return true;
+    }
+
+    @Override
+    public void handleReturnValue(Object returnValue, MethodParameter returnType, TelegramRequest telegramRequest) throws Exception {
+        Object outputValue;
+        Class<?> valueType;
+
+        if (returnValue instanceof CharSequence) {
+            outputValue = returnValue.toString();
+        } else {
+            outputValue = returnValue;
+            valueType = getReturnValueType(outputValue, returnType);
+            if (conversionService.canConvert(valueType, String.class)) {
+                outputValue = conversionService.convert(outputValue, String.class);
+            } else if (conversionService.canConvert(returnType.getParameterType(), String.class)) {
+                outputValue = conversionService.convert(returnType.getParameterType(), String.class);
+            }
+        }
+        telegramRequest.setBaseRequest(new SendMessage(telegramRequest.chatId(), (String) outputValue));
+    }
+
+    private Class<?> getReturnValueType(Object value, MethodParameter returnType) {
+        return (value != null ? value.getClass() : returnType.getParameterType());
+    }
+
+}
---
+++ sample/src/main/java/TourBotApp.java
@@ -0,0 +1,10 @@
+/*
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourBotApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TourBotApp.class);
+    }
+}*/
